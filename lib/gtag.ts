ff89e11a3eedc84aba45b9f75427cc0bdea5632d
AGupta <agupta@boomerangfx.com>
2025-03-30 21:35:12 +0530

    `Reverted Google Analytics configuration`

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,7 +1,7 @@
-import { CONFIG } from '../config/blog'
+export const GA_TRACKING_ID = process.env.GOOGLE_ANALYTICS_VIEW_ID || "G-HGPG08YYC4"
 
 export const pageview = (url: string, title: string) => {
-  window.gtag('config', CONFIG.GA_TRACKING_ID, {
+  window.gtag('config', GA_TRACKING_ID, {
     page_location: url,
     page_title: title,
   })
