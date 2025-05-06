b25387c14f51094a3748cd6c8cca9420382ce23b
AGupta <agupta@boomerangfx.com>
2025-05-06 15:33:29 +0530

    `Refactor categories and tags pages`

--- pages/tags.tsx
+++ pages/tags.tsx
@@ -7,71 +7,60 @@ import { Colors, getColorClassByName } from '../lib/colors'
 import { NextPageWithLayout } from './_app'
 import { getPosts } from '../lib/apis'
 import { getAllSelectItemsFromPosts } from '../lib/apis/getAllSelectItemsFromPosts'
-import { TTags } from '../types'
+import { TagCardProps, TTags } from '../types'
 import { NextSeo } from 'next-seo'
 import { CONFIG } from '../config/blog'
 import { useRouter } from 'next/router'
 import { filterPosts } from '../lib/apis/filterPosts'
 
-const TagCard = ({
-  name,
-  color,
-  count,
-}: {
-  name: string
-  color: string
-  count: number
-}) => {
+const TagCard: React.FC<TagCardProps> = ({ name, color, count }) => {
   return (
-    <Link href="/tags/[name]" as={`/tags/${name}`} key={name}>
-      <a>
-        <div
-          className={`${
-            Colors[color]?.bg.msg ?? Colors['gray'].bg.msg
-          } bg-gradient-to-bl from-white/30 text-white flex items-center font-semibold py-3 px-5  rounded-full justify-between transform transition ease-in-out duration-200 hover:scale-95`}
-          dark="bg-gradient-to-tr from-black/30"
-        >
-          <p className="line-clamp-1"># {name}</p>
-          <p
-            className={`bg-white ${
-              Colors[color]?.text.normal ?? Colors['gray'].text.normal
-            } text-sm px-1.5 text-center rounded-full`}
-          >
-            {count}
-          </p>
-        </div>
-      </a>
-    </Link>
+    <Link href={`/tags/${name}`}
+      className={`${Colors[color]?.bg.msg ?? Colors['gray'].bg.msg
+        } bg-gradient-to-bl from-white/30 text-white flex items-center font-semibold py-3 px-5  rounded-full justify-between transform transition ease-in-out duration-200 hover:scale-95`}
+      dark="bg-gradient-to-tr from-black/30"
+    >
+      <p className="line-clamp-1"># {name}</p>
+      <p
+        className={`bg-white ${Colors[color]?.text.normal ?? Colors['gray'].text.normal
+          } text-sm px-1.5 text-center rounded-full`}
+      >
+        {count}
+      </p>
+    </Link >
   )
 }
 
 const Tags: NextPage<{ tags: TTags }> = ({ tags }) => {
   const router = useRouter()
+
   return (
     <>
       <NextSeo
         title={`Tags | ${CONFIG.BLOG_TITLE}`}
         canonical={router.asPath}
-        description={`tags in morethanmin's blog`}
+        description={`Tags in ${CONFIG.BLOG_TITLE}'s blog`}
         openGraph={{
           title: `${CONFIG.BLOG_TITLE}`,
-          description: `tags in morethanmin's blog`,
+          description: `Tags in ${CONFIG.BLOG_TITLE}'s blog`,
           locale: router.locale,
           type: 'website',
           url: `${router.asPath}`,
           // images: [featuredImage],
         }}
       />
+
       <ListLayout>
         <h1 className="mb-4 text-2xl font-bold md:text-3xl lg:mb-8">Tags✨</h1>
         <div className="grid grid-cols-1 xs:grid-cols-2 gap-3 pb-4 lg:pb-8 md:grid-cols-3 lg:grid-cols-4">
-          {Object.keys(tags).map((tagName) =>
-            TagCard({
-              name: tagName,
-              color: getColorClassByName(tagName),
-              count: tags[tagName],
-            })
-          )}
+          {Object.entries(tags).map(([tagName, count]) => (
+            <TagCard
+              key={tagName}
+              name={tagName}
+              color={getColorClassByName(tagName)}
+              count={count}
+            />
+          ))}
         </div>
       </ListLayout>
     </>
@@ -90,10 +79,8 @@ export const getStaticProps: GetStaticProps = async () => {
     revalidate: 60 * 60,
   }
 }
-;(Tags as NextPageWithLayout).getLayout = function getLayout(
-  page: ReactElement
-) {
-  return <BlogLayoutPure>{page}</BlogLayoutPure>
-}
+  ; (Tags as NextPageWithLayout).getLayout = function getLayout(page: ReactElement) {
+    return <BlogLayoutPure>{page}</BlogLayoutPure>
+  }
 
 export default Tags
--- types/index.ts
+++ types/index.ts
@@ -1,27 +1,34 @@
-export type TPostStatus = 'Private' | 'Public'
+export type TPostStatus = "Private" | "Public";
 
 export type TPost = {
-  id: string
-  date: { start_date: string }
-  slug: string
-  tags?: string[]
-  category?: string[]
-  summary?: string
-  title: string
-  status: TPostStatus[]
-  createdTime: string
-  fullWidth: boolean
-  thumbnail?: string
-  cover?: string
-}
+  id: string;
+  date: { start_date: string };
+  slug: string;
+  tags?: string[];
+  category?: string[];
+  summary?: string;
+  title: string;
+  status: TPostStatus[];
+  createdTime: string;
+  fullWidth: boolean;
+  thumbnail?: string;
+  cover?: string;
+};
 
-export type TPosts = TPost[]
+export type TPosts = TPost[];
 
 export type TTags = {
-  [tagName: string]: number
-}
-export type TCategories = {
-  [category: string]: number
-}
+  [tagName: string]: number;
+};
 
-export type ThemeType = 'dark' | 'light'
+export type TagCardProps = {
+  name: string;
+  color: string;
+  count: number;
+};
+
+export type TCategories = { [category: string]: number };
+export type CategoriesProps = { categories: TCategories };
+export type CategoryCardProps = { name: string; color: string; count: number };
+
+export type ThemeType = "dark" | "light";
