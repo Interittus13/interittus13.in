5f2ed93e993a86a71bfb5e9d9d86e03b6b55b458
Interittus13 <arpit.gupta.0121@gmail.com>
2026-03-30 10:35:36 +0530

    fix(layout): fix Tailwind 4 dark mode

--- src/components/layout/BlogLayout.tsx
+++ src/components/layout/BlogLayout.tsx
@@ -5,7 +5,7 @@ import Footer from '../layout/Footer'
 export default function BlogLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <div className="bg-true-gray-50 dark:bg-black">
+      <div className="bg-zinc-50 dark:bg-zinc-950 transition-colors duration-500">
         <Navbar />
         <main>{children}</main>
         <div className="mt-auto">
